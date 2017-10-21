de08ecca2bc5d4af4854c2d4bef02227cedc8436
Tshepo Mahlaula <tshepo.mahlaula@vcontractor.co.za>
2017-10-21 12:02:50 +0200

    Added date and time class

--- app/src/main/java/com/emgr/geartronix/providers/DateTimeProvider.java
+++ app/src/main/java/com/emgr/geartronix/providers/DateTimeProvider.java
@@ -6,33 +6,46 @@ import java.util.Date;
 public class DateTimeProvider {
 
     private SimpleDateFormat formatedDate;
-    private int day, hour, minute;
+    private int dateTime, time, day, hour, minute;
+    private final String DAYFORMAT = "D";
     private final String HOURFORMAT = "H";
     private final String MINUTEFORMAT = "M";
-    private final String SECONDFORMAT = "S";
+    private final String TIMEFORMAT = "H:M:S";
+    private final String DATETIMEFORMAT = "D-M-Y H:M:S";
 
     public DateTimeProvider () {
 
     }
 
-    public void getCurrentDateAndTime() {
-        formatDate("D-M-Y");
-        formatedDate.format(new Date());
+    public String getCurrentDateAndTime() {
+        return formatAndSetTime(DATETIMEFORMAT, dateTime);
     }
 
-    public void getCurrentTime() {
-        formatDate("H:M:S");
-        formatedDate.format(new Date());
+    public String getCurrentTime() {
+        return formatAndSetTime(TIMEFORMAT, time);
     }
 
-    public void getCurrentHour() {
-        formatDate(HOURFORMAT);
-        hour = Integer.parseInt(formatedDate.format(new Date()));
+    public String getCurrentDay() {
+        return formatAndSetTime(DAYFORMAT, day);
     }
 
-    public void getCurrentMinute() {
-        formatDate(MINUTEFORMAT);
-        minute = Integer.parseInt(formatedDate.format(new Date()));
+    public String getCurrentHour() {
+        return formatAndSetTime(HOURFORMAT, hour);
+    }
+
+    public String getCurrentMinute() {
+        return formatAndSetTime(MINUTEFORMAT, minute);
+    }
+
+    public void setTimeUnit(int timeUnit) {
+        timeUnit = Integer.parseInt(formatedDate.format(new Date()));
+    }
+
+    public String formatAndSetTime(String format, int timeUnit) {
+        formatDate(format);
+        setTimeUnit(timeUnit);
+
+        return formatedDate.toString();
     }
 
     private void formatDate(String format) {
