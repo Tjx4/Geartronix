f926a02cb8f799df9e7c6c2ac45f7772135f5556
Tshepo Mahlaula <tshepo.mahlaula@vcontractor.co.za>
2017-10-21 11:41:18 +0200

    Added providers

---
+++ app/src/main/java/com/emgr/geartronix/views/ILoginView.java
@@ -0,0 +1,9 @@
+package com.emgr.geartronix.views;
+
+import android.view.View;
+
+public interface ILoginView {
+    void onLoginButtonClicked(View view);
+    void onRegisterButtonClicked(View view);
+    void onForgotPasswordClicked(View view);
+}
