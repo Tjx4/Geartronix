144d7a7357cff1b8cbfff45b76bf4d629f069a12
Tshepo Mahlaula <tshepo.mahlaula@vcontractor.co.za>
2017-11-14 16:41:30 +0200

    Added style to menu

--- app/src/main/java/com/emgr/geartronix/presenters/HomePresenter.java
+++ app/src/main/java/com/emgr/geartronix/presenters/HomePresenter.java
@@ -31,11 +31,10 @@ public class HomePresenter extends BaseAsyncPresenter implements IHomePresenter 
             showShortToast("Welcome "+user);
 
 
-
+            slideOutActivity();
         }
         catch (Exception e){
 
-            slideOutActivity();
         }
 
     }
@@ -124,19 +123,6 @@ showShortToast("handleNavigationItemSelected");
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
