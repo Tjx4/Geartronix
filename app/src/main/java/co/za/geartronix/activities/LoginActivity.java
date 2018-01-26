e2baeec60322069533c1b0eeaf89e62e9a51ab06
Tshepo Mahlaula <tshepo.mahlaula@vcontractor.co.za>
2018-01-26 21:11:57 +0200

    fixed registration side menu

--- app/src/main/java/co/za/geartronix/activities/LoginActivity.java
+++ app/src/main/java/co/za/geartronix/activities/LoginActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import co.za.geartronix.R;
 import co.za.geartronix.presenters.LoginPresenter;
 import co.za.geartronix.views.ILoginView;
--- app/src/main/java/co/za/geartronix/activities/RegistrationActivity.java
+++ app/src/main/java/co/za/geartronix/activities/RegistrationActivity.java
@@ -1,6 +1,8 @@
 package co.za.geartronix.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import co.za.geartronix.R;
 import co.za.geartronix.presenters.RegistrationPresenter;
 import co.za.geartronix.views.IRegistrationView;
@@ -11,6 +13,18 @@ public class RegistrationActivity extends BaseSlideMenuActivity implements IRegi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setMenu(R.menu.registration_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.registration_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        getPresenter().menuOptionSelected(item);
+        return true;
     }
 
     @Override
